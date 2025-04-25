43aa544d3f91258fddafe2a8ed3062610e8f2b1e
emolesky <emilynnmolesky@gmail.com>
2025-04-24 20:49:03 -0500

    Add files via upload

---
+++ Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeros
+{
+    internal class Solution
+    {
+        public void MoveZeros(int[] nums)
+        {
+            int insertPos = 0;
+
+            for(int i = 0; i< nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[insertPos] = nums[i];
+                    insertPos++;
+                }
+            }
+            for(int i = insertPos; i < nums.Length;i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}

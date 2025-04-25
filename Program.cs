32d1905c04c6c9546ef270514ee00cd4943e0f13
emolesky <emilynnmolesky@gmail.com>
2025-04-24 20:50:44 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace Majority
+﻿namespace HappyNumber
 {
     internal class Program
     {
--- Solution.cs
+++ Solution.cs
@@ -4,24 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Majority
+namespace HappyNumber
 {
     internal class Solution
     {
-        public int MajorityElement(int[] nums)
-        {
-            int count = 0;
-            int s = 0;
-            foreach(int num in nums)
-            {
-                if (count==0)
-                {
-                    s = num;
-                }
-                count += (num == s) ? 1 : -1;
-
-            }
-            return s;
-        }
     }
 }
